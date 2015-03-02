29994438c179462d371d3755b53a457d282aa694
suhyuk-park <park.suhyuk@gmail.com>
2015-03-03 00:12:13 +0900

    first update

--- Assets/Scripts/HelloUnity3d.cs
+++ Assets/Scripts/HelloUnity3d.cs
@@ -5,7 +5,7 @@ public class HelloUnity3d : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Hello Unity3d");
+		Debug.Log ("Hello Unity3d World.");
 	}
 	
 	// Update is called once per frame
